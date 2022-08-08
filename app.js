1277c53e11d0e2e9de1871d816d1cfce7c2454b3
SongYong1412 <994225036@qq.com>
2022-08-08 18:24:42 +0800

    跨域配置

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 const json = require('koa-json')
 const bodyparser = require('koa-bodyparser')
+const cors = require('koa2-cors')
 
 const db = require('./config/db')
 const indexRoute = require('./routes/index')
@@ -12,6 +13,18 @@ const roleRoute = require('./routes/role')
 const fileRoute = require('./routes/file')
 
 const { pageFilter } = require('./common/common')
+
+//跨域配置
+app.use(cors({
+  origin: function(ctx) {
+    return 'http://localhost:3000';
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
 
 //数据库连接
 db()
